c03e01e8af62fffc80c50d2f0ae4e731ba32abe9
Ahmed Samy <69104685+ahmedsamidev@users.noreply.github.com>
2024-10-30 16:04:20 +0300

    Time: 0 ms (100%), Space: 5.9 MB (6.85%) - LeetHub

--- 0001-two-sum/0001-two-sum.go
+++ 0001-two-sum/0001-two-sum.go
@@ -1,14 +1,13 @@
-func twoSum(nums []int, target int) []int {
-    result := make([]int, 2)
-    for i := 0; i < len(nums); i++{
-        for j := i+1; j < len(nums); j++{
-            if nums[i] + nums[j] == target{
-                result[0] = i
-                result[1] = j
-                return result
-            }
-        }
-    }
-    return  result
 
+
+func twoSum(nums []int, target int) []int {
+    numMap := make(map[int]int)
+    for i, num := range nums {
+        complement := target - num
+        if j, found := numMap[complement]; found {
+            return []int{j, i}
+        }
+        numMap[num] = i
+    }
+    return nil
 }
