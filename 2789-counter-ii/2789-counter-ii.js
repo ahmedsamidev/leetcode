13840e09a07f543370d38d40f37c6f078781e20e
Ahmed Samy <69104685+ahmedsamidev@users.noreply.github.com>
2025-04-14 10:57:56 +0200

    Time: 62 ms (6.78%), Space: 55.2 MB (88.46%) - LeetHub

---
+++ 2789-counter-ii/2789-counter-ii.js
@@ -0,0 +1,33 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function (init) {
+
+
+    let val = init
+
+     const increment = function () {
+        return ++val;
+    };
+
+    const decrement = function () {
+        return --val;
+    };
+    const reset = function () {
+        val = init
+        return val
+    }
+
+
+    return { increment, decrement, reset }
+
+
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
